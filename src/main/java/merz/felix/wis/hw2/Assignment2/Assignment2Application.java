6e0983eef6589c8fd98dbff016bfed9c0b5cb719
Felix Merz <felix-merz@web.de>
2018-06-14 23:31:32 +0200

    initial and final commit

---
+++ src/main/java/merz/felix/wis/hw2/Assignment2/Assignment2Application.java
@@ -0,0 +1,20 @@
+package merz.felix.wis.hw2.Assignment2;
+
+import java.util.HashMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment2Application {
+	public static HashMap<Long,Student> hmStudent;
+	
+	public static void main(String[] args) {
+		hmStudent = new HashMap<Long, Student>();
+		
+		Student one = new Student("Felix Merz", "WIS");
+		hmStudent.put(new Long(one.getId()), one);
+		
+		SpringApplication.run(Assignment2Application.class, args);
+	}
+}
